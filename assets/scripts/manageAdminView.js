b877d8964e95113045ae0e0fd8f30073508db474
SAitharaju <sindhu.aitharaju@gmail.com>
2016-10-19 22:01:07 +0200

    Update manageAdminView.js

--- assets/scripts/manageAdminView.js
+++ assets/scripts/manageAdminView.js
@@ -12,15 +12,15 @@ function rand(min, max) {
 	$(document).ready(function() {
 		
 		var color = "#5f0e88";
-
+		//reset admin view when the admin view is opened
 		$('#adminlink').click(function() { 
 			resetAdminView();
 		});
-
+		
 		$('#add-designer-admin-email').on('change keyup paste',function() { 
 			$('#email-added-msg').text("");
 			var val = $.trim(this.value);
-			if(val!= ""){
+			if(val!= ""){ //enable or disable Add button by validating the text field value
 				$('#add-designer-email').find('*').prop('disabled',false);
 				$('#add-designer-email').find('*').removeClass('ui-disabled');
 			} else{
@@ -43,13 +43,13 @@ function rand(min, max) {
 			}
 			
 			formdata.append("oidcEmail",email);
-			if(email == 'a.herrler@maastrichtuniversity.nl' || email == 'sindhu.aitharaju@rwth-aachen.de'){
-				formdata.append("admin","true");
+			if(email == 'a.herrler@maastrichtuniversity.nl' || email == 'sindhu.aitharaju@rwth-aachen.de' || email == 'klamma@dbis.rwth-aachen.de'){
+				formdata.append("admin","true"); //Give write permissions to everything
 			}else{
 				formdata.append("admin","part");
 		}
 
-			if(formdata){
+			if(formdata){ // Add the email to the database
 				$.ajax({
 					url: "lib/database/create_assessment_admin.php",
 					type: "POST",
@@ -60,7 +60,7 @@ function rand(min, max) {
 						resetAdminView();
 						$('#email-added-msg').text("");
 						var emailAdded = $('<h2 style="color:#48036b;">Email added successfully!</h2>');
-						$('#email-added-msg').append(emailAdded);
+						$('#email-added-msg').append(emailAdded); //display success message
 
 
 					}
@@ -71,7 +71,7 @@ function rand(min, max) {
 
 	});
 
-	function resetAdminView(){
+	function resetAdminView(){ //reset the view
 		$('#email-added-msg').text("");
 		$('#add-designer-admin-email')[0].value = "";
 		setButtonColor($('#add-designer-email'));
@@ -80,7 +80,7 @@ function rand(min, max) {
 
 	}
 
-	function setButtonColor(divName){
+	function setButtonColor(divName){ //Set colour to the buttons in this view
 		var color = "#5f0e88";
 			if (divName.find('*').hasClass('ui-btn-inner')) {
 				divName.find('*').css("color",color);
